edd183a3e04811e26ae202f14e68ec4c1e22e039
Sanberqui18 <119998036+Sanberqui18@users.noreply.github.com>
2024-02-05 19:40:18 -0500

    Write Tests (#1)

---
+++ tests/home.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Home", () => {
+  test("Open HomePage and verify title", async ({ page }) => {
+    //open url
+    await page.goto("https://practice.sdetunicorns.com/");
+
+    //verify title
+    await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns");
+  });
+
+  test("Open About Page and verify title", async ({ page }) => {
+    //open url
+    await page.goto("https://practice.sdetunicorns.com/about/");
+
+    //verify title
+    await expect(page).toHaveTitle("About – Practice E-Commerce Site");
+  });
+});
