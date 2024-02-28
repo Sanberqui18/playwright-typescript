203b8619e75a80fa87a7431bfaa1d94ebae4f664
Santiago Bernal <san.be.qui18@gmail.com>
2024-02-27 23:34:58 -0500

    Add global setup helpers

--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -1,7 +1,9 @@
 import { type FullConfig, chromium } from "@playwright/test";
 
+//https://playwright.dev/docs/test-global-setup-teardown#option-2-configure-globalsetup-and-globalteardown
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function globalSetup(config: FullConfig) {
+  //parameter used to access the config file and get baseURL, storageState, etc (const { baseURL, storageState } = config.projects[0].use;)
   const browser = await chromium.launch(); // as it is not in a test block, we should create the browser instance from scratch
   const page = await browser.newPage();
   await page.goto("https://practice.sdetunicorns.com/my-account");
